3de4dc6c4bc1707f4112b1321aa91073590c9efc
Alicia Akdogan <alicia.akdogan@hotmail.com>
2023-03-21 12:50:02 +0100

    added create

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -6,20 +6,27 @@ export default function Board() {
   return (
     <div className="board">
       <div className="board-column">
-        <div className="boars-title">
-          <div className="board-title icon">
-            <h4>Done</h4>
-          </div>
+        <div className="board-title">
+          <div className="board-title-icon todo"></div>
+          <h4>Todo</h4>
         </div>
         <Ticket />
         <Ticket />
         <Ticket />
       </div>
       <div className="board-column">
+        <div className="board-title">
+          <div className="board-title-icon doing"></div>
+          <h4>Doing</h4>
+        </div>
         <Ticket />
         <Ticket />
       </div>
       <div className="board-column">
+        <div className="board-title">
+          <div className="board-title-icon done"></div>
+          <h4>Done</h4>
+        </div>
         <Ticket />
         <Ticket />
         <Ticket />
