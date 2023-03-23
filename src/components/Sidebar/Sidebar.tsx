90fff053f07f361b8abc77070cbcae897d56f1df
Alicia Akdogan <alicia.akdogan@hotmail.com>
2023-03-23 16:16:26 +0100

    made it more responsive

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,54 +1,17 @@
-import React, { useState } from "react";
-import { Folder, Plus } from "react-feather";
 import "./Sidebar.scss";
 import { useBoardContext } from "../../context/BoardsContext";
+import ProjectsLinks from "../ProjectsLinks/ProjectsLinks";
 
 type Props = {};
 
 export default function Sidebar({}: Props) {
-    const { projects, createProject, currentProject, changeBoard } =
-        useBoardContext();
-    const [showCreateProject, setShoeCreateProject] = useState(false);
-    const [projectName, setProjectName] = useState("");
+  const { projects, createProject, currentProject, changeBoard } =
+    useBoardContext();
 
-    return (
-        <div className="sidebar">
-            <p>Allboards ({projects.length}) </p>
-            <ul className="sidebar-items">
-                {projects.map((project, index) => (
-                    <li
-                        className={
-                            project.id === currentProject.id ? "active" : ""
-                        }
-                        onClick={() => changeBoard(index)}
-                    >
-                        <Folder />
-                        {project.name}
-                    </li>
-                ))}
-
-                <li onClick={() => setShoeCreateProject(!showCreateProject)}>
-                    <Plus />
-                    Create board
-                </li>
-            </ul>
-            {showCreateProject && (
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        createProject(projectName);
-                        setProjectName("");
-                        setShoeCreateProject(false);
-                    }}
-                >
-                    <input
-                        type="text"
-                        value={projectName}
-                        onChange={(e) => setProjectName(e.target.value)}
-                    />
-                    <button className="btn">Create</button>
-                </form>
-            )}
-        </div>
-    );
+  return (
+    <div className="sidebar">
+      <p>Allboards ({projects.length}) </p>
+      <ProjectsLinks />
+    </div>
+  );
 }
