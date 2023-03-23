dfea013a9f4c7850085f690f7b771761c83d2605
Alicia Akdogan <alicia.akdogan@hotmail.com>
2023-03-23 16:16:05 +0100

    made it more responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,35 @@ import Navbar from "./components/Navbar/Navbar";
 import Board from "./components/Board/Board";
 import CreateTaskModel from "./components/CeateTaskModel/CreateTaskModel";
 import { useState } from "react";
+import ProjectsDropdown from "./components/ProjectsDropdown/ProjectsDropdown";
 export default function App() {
   const [showCreateTask, setShowCreateTask] = useState(false);
+  const [showProjectDropdown, setShowProjectDropdown] = useState(false);
 
   function toggleShowCreateTask() {
     setShowCreateTask(!showCreateTask);
   }
+
+  function toggleShowProjectDropdown() {
+    setShowProjectDropdown(!showProjectDropdown);
+  }
   return (
     <div className="app">
-      <Navbar toggleShowCreateTask={toggleShowCreateTask} />
+      <Navbar
+        toggleShowCreateTask={toggleShowCreateTask}
+        toggleShowProjectsDropdown={toggleShowProjectDropdown}
+      />
       <div className="main-container">
         <Sidebar />
         <Board />
       </div>
       {showCreateTask && (
         <CreateTaskModel toggleShowCreateTask={toggleShowCreateTask} />
+      )}
+      {showProjectDropdown && (
+        <ProjectsDropdown
+          toggleShowProjectsDropdown={toggleShowProjectDropdown}
+        />
       )}
     </div>
   );
