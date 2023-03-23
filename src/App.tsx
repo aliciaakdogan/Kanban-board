121be9dec1c7dba85e7fed02959ffe4d622c28f1
Alicia Akdogan <alicia.akdogan@hotmail.com>
2023-03-23 11:30:08 +0100

    added crate ticket

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import Sidebar from "./components/Sidebar/Sidebar";
 import Navbar from "./components/Navbar/Navbar";
 import Board from "./components/Board/Board";
+import CreateTaskModel from "./components/CeateTaskModel/CreateTaskModel";
+import { useState } from "react";
 export default function App() {
+  const [showCreateTask, setShowCreateTask] = useState(false);
+
+  function toggleShowCreateTask() {
+    setShowCreateTask(!showCreateTask);
+  }
   return (
     <div className="app">
-      <Navbar />
+      <Navbar toggleShowCreateTask={toggleShowCreateTask} />
       <div className="main-container">
         <Sidebar />
         <Board />
       </div>
+      {showCreateTask && (
+        <CreateTaskModel toggleShowCreateTask={toggleShowCreateTask} />
+      )}
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -6,47 +6,49 @@ import { useBoardContext } from "../../context/BoardsContext";
 type Props = {};
 
 export default function Sidebar({}: Props) {
-  const { projects, createProject, currentProject, changeBoard } =
-    useBoardContext();
-  const [showCreateProject, setShoeCreateProject] = useState(false);
-  const [projectName, setProjectName] = useState("");
+    const { projects, createProject, currentProject, changeBoard } =
+        useBoardContext();
+    const [showCreateProject, setShoeCreateProject] = useState(false);
+    const [projectName, setProjectName] = useState("");
 
-  return (
-    <div className="sidebar">
-      <p>Allboards (2) </p>
-      <ul className="sidebar-items">
-        {projects.map((project, index) => (
-          <li
-            className={project.id === currentProject.id ? "active" : ""}
-            onClick={() => changeBoard(index)}
-          >
-            <Folder />
-            {project.name}
-          </li>
-        ))}
+    return (
+        <div className="sidebar">
+            <p>Allboards ({projects.length}) </p>
+            <ul className="sidebar-items">
+                {projects.map((project, index) => (
+                    <li
+                        className={
+                            project.id === currentProject.id ? "active" : ""
+                        }
+                        onClick={() => changeBoard(index)}
+                    >
+                        <Folder />
+                        {project.name}
+                    </li>
+                ))}
 
-        <li onClick={() => setShoeCreateProject(!showCreateProject)}>
-          <Plus />
-          Create board
-        </li>
-      </ul>
-      {showCreateProject && (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            createProject(projectName);
-            setProjectName("");
-            setShoeCreateProject(false);
-          }}
-        >
-          <input
-            type="text"
-            value={projectName}
-            onChange={(e) => setProjectName(e.target.value)}
-          />
-          <button className="btn">Create</button>
-        </form>
-      )}
-    </div>
-  );
+                <li onClick={() => setShoeCreateProject(!showCreateProject)}>
+                    <Plus />
+                    Create board
+                </li>
+            </ul>
+            {showCreateProject && (
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        createProject(projectName);
+                        setProjectName("");
+                        setShoeCreateProject(false);
+                    }}
+                >
+                    <input
+                        type="text"
+                        value={projectName}
+                        onChange={(e) => setProjectName(e.target.value)}
+                    />
+                    <button className="btn">Create</button>
+                </form>
+            )}
+        </div>
+    );
 }
