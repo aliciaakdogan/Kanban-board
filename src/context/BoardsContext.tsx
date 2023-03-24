b042064e0093951ca53165baeac6d9801118e9a0
Alicia Akdogan <alicia.akdogan@hotmail.com>
2023-03-24 10:44:28 +0100

    selected index is now stored in localstorage

--- src/context/BoardsContext.tsx
+++ src/context/BoardsContext.tsx
@@ -4,90 +4,102 @@ import { Project, projectsData } from "./projects-data";
 import { useLocalStorage } from "usehooks-ts";
 
 interface Context {
-    projects: Project[];
-    createProject: (name: string) => void;
-    changeBoard: (index: number) => void;
-    createTicket: (
-        title: string,
-        description: string,
-        subtasks: string[],
-        index: number
-    ) => void;
-    currentProject: Project;
+  projects: Project[];
+  createProject: (name: string) => void;
+  changeBoard: (index: number) => void;
+  createTicket: (
+    title: string,
+    description: string,
+    subtasks: string[],
+    index: number
+  ) => void;
+  changeCurrentProjectBoard: (board: Project["board"]) => void;
+  currentProject: Project;
 }
 
 const BoardsContext = createContext<Context>({
-    projects: [],
-    createProject: () => {},
-    changeBoard: () => {},
-    createTicket: () => {},
-    currentProject: {} as Project,
+  projects: [],
+  createProject: () => {},
+  changeBoard: () => {},
+  createTicket: () => {},
+  changeCurrentProjectBoard: () => {},
+  currentProject: {} as Project,
 });
 
 export function useBoardContext() {
-    return useContext(BoardsContext);
+  return useContext(BoardsContext);
 }
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 export default function BoardsContextProvider({ children }: Props) {
-    const [projects, setProjects] = useLocalStorage<Project[]>(
-        "project-data",
-        projectsData
-    );
+  const [projects, setProjects] = useLocalStorage<Project[]>(
+    "project-data",
+    projectsData
+  );
 
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const currentProject = projects[selectedIndex];
+  const [selectedIndex, setSelectedIndex] = useLocalStorage(
+    "selected-index",
+    0
+  );
+  const currentProject = projects[selectedIndex];
 
-    function createProject(name: string) {
-        setProjects([
-            ...projects,
-            {
-                name,
-                id: uuidv4(),
-                board: [
-                    { name: "Todo", tickets: [] },
-                    { name: "Doing", tickets: [] },
-                    { name: "Done", tickets: [] },
-                ],
-            },
-        ]);
-        changeBoard(projects.length);
-    }
+  function createProject(name: string) {
+    setProjects([
+      ...projects,
+      {
+        name,
+        id: uuidv4(),
+        board: [
+          { name: "Todo", tickets: [] },
+          { name: "Doing", tickets: [] },
+          { name: "Done", tickets: [] },
+        ],
+      },
+    ]);
+    changeBoard(projects.length);
+  }
 
-    function changeBoard(index: number) {
-        setSelectedIndex(index);
-    }
+  function changeBoard(index: number) {
+    setSelectedIndex(index);
+  }
 
-    function createTicket(
-        title: string,
-        description: string,
-        subtasks: string[],
-        index: number
-    ) {
-        projects[selectedIndex].board[index].tickets.push({
-            title,
-            description,
-            tasks: subtasks,
-            id: uuidv4(),
-        });
+  function changeCurrentProjectBoard(board: Project["board"]) {
+    projects[selectedIndex].board = board;
 
-        setProjects([...projects]);
-    }
+    setProjects([...projects]);
+  }
 
-    return (
-        <BoardsContext.Provider
-            value={{
-                projects,
-                createProject,
-                currentProject,
-                changeBoard,
-                createTicket,
-            }}
-        >
-            {children}
-        </BoardsContext.Provider>
-    );
+  function createTicket(
+    title: string,
+    description: string,
+    subtasks: string[],
+    index: number
+  ) {
+    projects[selectedIndex].board[index].tickets.push({
+      title,
+      description,
+      tasks: subtasks,
+      id: uuidv4(),
+    });
+
+    setProjects([...projects]);
+  }
+
+  return (
+    <BoardsContext.Provider
+      value={{
+        projects,
+        createProject,
+        currentProject,
+        changeBoard,
+        createTicket,
+        changeCurrentProjectBoard,
+      }}
+    >
+      {children}
+    </BoardsContext.Provider>
+  );
 }
